880588ad4f8f9429c6e34e376228f795de580c61
sufailbadushat <sufailbadushat@gamil.com>
2022-11-22 16:22:33 +0530

    json code added

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,5 @@
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -17,19 +19,23 @@ public class Student {
     public static void main(String[] args) {
         int condition;
         ArrayList<Student> studentArr = new ArrayList<>();
+        ArrayList<String> jsonArr = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
+
+
         while (true){
             System.out.println("---------------------Operation Menu--------------------------------");
             System.out.println("1.Add a student");
             System.out.println("2.View all students");
             System.out.println("3.Search a student with admission number and show all details");
             System.out.println("4.Delete a student with admission number");
-            System.out.println("5.Exit");
+            System.out.println("5.Print Json Array");
+            System.out.println("6.Exit");
             System.out.println("------------------------------------------------------------------");
             System.out.println("Enter your choice: ");
             condition = sc.nextInt();
-            switch (condition){
-                case 1:
+            switch (condition) {
+                case 1 -> {
                     System.out.println("Add student details");
                     System.out.println("Enter admission number:");
                     int adNo = sc.nextInt();
@@ -39,49 +45,48 @@ public class Student {
                     int rollNo = sc.nextInt();
                     System.out.println("Enter college name:");
                     String collegeName = sc.next();
-
                     Student student = new Student(adNo, name, rollNo, collegeName);
                     studentArr.add(student);
-                    break;
-                case 2 :
+                    jsonArr.add(new Gson().toJson(student));
+                }
+                case 2 -> {
                     System.out.println("Student details");
-                    for (Student student1 : studentArr){
-                        System.out.println("Ad Number: "+student1.adNo);
-                        System.out.println("Name: "+student1.name);
-                        System.out.println("Roll Number: "+student1.rollNo);
-                        System.out.println("College Name: "+student1.collegeName);
+                    for (Student student1 : studentArr) {
+                        System.out.println("Ad Number: " + student1.adNo);
+                        System.out.println("Name: " + student1.name);
+                        System.out.println("Roll Number: " + student1.rollNo);
+                        System.out.println("College Name: " + student1.collegeName);
                     }
-                    break;
-                case 3 :
+                }
+                case 3 -> {
                     System.out.println("Enter the admission number");
                     int adNo1 = sc.nextInt();
-                    for(Student student1 : studentArr){
-                        if(adNo1==student1.adNo){
+                    for (Student student1 : studentArr) {
+                        if (adNo1 == student1.adNo) {
                             System.out.println("Student with admission number " + adNo1 + " is " + student1.name);
                             break;
-                        }
-                        else {
+                        } else {
                             System.out.println("No such student found");
                         }
                     }
-                    break;
-                case 4 :
+                }
+                case 4 -> {
                     System.out.println("Enter the admission number");
                     int adNo2 = sc.nextInt();
-                    for (Student student1 : studentArr){
-                        if(adNo2==student1.adNo){
+                    for (Student student1 : studentArr) {
+                        if (adNo2 == student1.adNo) {
 
                             studentArr.remove(student1);
-                            System.out.println("Student with admission number "+adNo2+" has been removed.");
+                            System.out.println("Student with admission number " + adNo2 + " has been removed.");
                             break;
                         }
                     }
-                    break;
-                case 5:
-                    System.exit(0);
-                default:
-                    System.out.println("Enter valid operation number");
-
+                }
+                case 5 -> {
+                    System.out.println(jsonArr);
+                }
+                case 6 -> System.exit(0);
+                default -> System.out.println("Enter valid operation number");
             }
         }
     }
